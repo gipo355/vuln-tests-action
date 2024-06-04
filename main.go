aa2d4b4e2e70c8a6f0ccde7b041210d9be68eb12
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-04 17:30:16 +0200

    WIP main.go

--- main.go
+++ main.go
@@ -22,14 +22,42 @@ func main() {
 	printTime()
 	printHello(args)
 
-	ls()
+	println("ls .")
+	ls(".")
+	println("ls ..")
+	ls("..")
+	println("ls /")
+	ls("/")
+	println("ls $HOME")
+	ls(os.Getenv("HOME"))
+	println("ls $GITHUB_WORKSPACE")
+	ls(os.Getenv("GITHUB_WORKSPACE"))
+	// RUNNER_WORKSPACE
+	println("ls $RUNNER_WORKSPACE")
+	ls(os.Getenv("RUNNER_WORKSPACE"))
 	printPwd()
 	printEnv()
+	printFile(githubOutput)
+	appendFile(githubOutput, fmt.Sprintf("arg=%v", args[0]))
+	printFile(githubOutput)
 }
 
 func printHello(args []string) {
 	name := args[0]
 	log.Printf("Hello, %v!", name)
+}
+
+func appendFile(path, content string) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0o644)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func printPwd() {
@@ -46,14 +74,29 @@ func printEnv() {
 	}
 }
 
-func ls() {
-	cmd := exec.Command("ls")
+func ls(path string) {
+	cmd := exec.Command("ls", "-la", path)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("File: %v", fi.Name())
 }
 
 func printTime() {
