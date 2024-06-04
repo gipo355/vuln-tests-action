cbe68e482a21f86c2f839ee4e82ad7ccca89c6a5
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-04 14:08:10 +0200

    WIP tentative fix for image

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,6 +15,16 @@ func main() {
 	}
 
 	name := args[0]
+
+	githubOutput := os.Getenv("GITHUB_OUTPUT")
+
+	date := time.Now().Format("2006-01-02 15:04:05")
+
+	newEnv := fmt.Sprintf("\ndate=%s", date)
+
+	newGithubOutput := githubOutput + newEnv
+
+	os.Setenv("GITHUB_OUTPUT", newGithubOutput)
 
 	log.Printf("Hello, %v!", name)
 }
