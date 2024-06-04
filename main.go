06f29efe3ec2609b8a4bb786fe7e8b7c5555413f
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-04 14:27:14 +0200

    WIP test go action with env vars

--- main.go
+++ main.go
@@ -16,15 +16,21 @@ func main() {
 
 	name := args[0]
 
+	// https://stackoverflow.com/questions/71357973/github-actions-set-two-output-names-from-custom-action-in-golang-code
 	githubOutput := os.Getenv("GITHUB_OUTPUT")
+	log.Printf("GITHUB_OUTPUT: %v", githubOutput)
 
 	date := time.Now().Format("2006-01-02 15:04:05")
+	// newEnv := fmt.Sprintf("\ndate=%s", date)
+	// newGithubOutput := githubOutput + newEnv
+	// os.Setenv("GITHUB_OUTPUT", newGithubOutput)
 
-	newEnv := fmt.Sprintf("\ndate=%s", date)
-
-	newGithubOutput := githubOutput + newEnv
-
-	os.Setenv("GITHUB_OUTPUT", newGithubOutput)
+	// fmt.Printf(`::set-output name=repo_tag::%s`, value)
+	// fmt.Print("\n")
+	// fmt.Printf(`::set-output name=ecr_tag::%s`, "v"+value)
+	// fmt.Print("\n")
+	fmt.Printf(`::set-output date=%s`, date)
+	fmt.Print("\n")
 
 	log.Printf("Hello, %v!", name)
 }
