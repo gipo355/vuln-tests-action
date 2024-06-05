dd60e233513b4eca9ffa18ac053f5124e41ce033
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-05 10:35:37 +0200

    WIP main

--- README.md
+++ README.md
@@ -19,3 +19,11 @@ The time we greeted you.
 uses: actions/hello-world-docker-action@v1
 with:
 who-to-greet: 'Mona the Octocat'
+
+## note for nmap
+
+we are using a docker container which allows us to pull and prepare needed tools for the scan.
+
+if we were to use a js action, we would have to install nmap on the runner machine which is not ideal.
+
+golang provides efficient concurrency and minimal image + mem size for containers using alpine.
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -209,5 +211,59 @@ func simpleNmap() {
 
 	if cmdErr := cmd.Run(); cmdErr != nil {
 		log.Panic(cmdErr)
+	}
+}
+
+func simpleNmapStream() {
+	host := GetInputEnv("HOST")
+
+	// this will execute the nmap, here we need to compose the command based on user input
+	// e.g. use scripts, flags, host, etc
+	cmd := exec.Command("nmap", "-sP", host)
+
+	file, fileErr := os.Create("nmap.log")
+	if fileErr != nil {
+		log.Panic(fileErr)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	// create a pipe to capture stdout and stderr
+	stdout, outPipeErr := cmd.StdoutPipe()
+	if outPipeErr != nil {
+		log.Panic(outPipeErr)
+	}
+
+	stderr, errPipeErr := cmd.StderrPipe()
+	if errPipeErr != nil {
+		log.Panic(errPipeErr)
+	}
+
+	// start the command
+	if cmdErr := cmd.Start(); cmdErr != nil {
+		log.Panic(cmdErr)
+	}
+
+	// create a goroutine to copy the stdout in a stream
+	go func() {
+		_, copyStdoutErr := io.Copy(writer, stdout)
+		if copyStdoutErr != nil {
+			log.Panic(copyStdoutErr)
+		}
+	}()
+
+	// create a goroutine to copy the stderr in a stream
+	go func() {
+		_, copyStderrErr := io.Copy(os.Stderr, stderr)
+		if copyStderrErr != nil {
+			log.Panic(copyStderrErr)
+		}
+	}()
+
+	// wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		log.Panic(err)
 	}
 }
